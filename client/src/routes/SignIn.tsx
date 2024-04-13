3be814b5e69d79dc45bf6705c008e5d0edac8625
erestes-22 <153776959+erestes-22@users.noreply.github.com>
2024-04-13 02:55:21 -0500

    SignUp done

--- client/src/routes/SignIn.tsx
+++ client/src/routes/SignIn.tsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
+
 function SignIn() {
+    const [userName, setUserName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [shown, setShown] = useState<boolean>(false);
+
+
+
     return (
-        <div className="flex flex-col gap-2">
-            <button className='text-green-500'><p>Green</p></button>
-            <button className='text-yellow-500'><p>Yellow</p></button>
-            <button className='text-red-500'><p>Red</p></button>
+        <div className="flex flex-col gap-2 bg-white h-screen items-center">
+          <div className="flex flex-col items-center gap-1">
+            <h1 className="mt-10 text-5xl font-bold text-red-700">
+                RazorFit
+            </h1>
+          </div>
+          <div className="text-3xl text-center "></div>
+            <hr></hr><hr></hr>
+          <div>
+            <input  
+              id="userName"
+              placeholder="UserName"
+              className="max-w-xs rounded-lg p-2 bg-slate-300 text-black"
+              value={userName}
+              onChange={(event) => setUserName(event.target.value)}
+            />
+            <br></br><br></br>
+            <input
+              id="pass"
+              placeholder="Password"
+              className="max-w-xs rounded-lg p-2 bg-slate-300 text-black"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+
+            {shown ? (
+                <p className="text-red-500"> Invalid credentials</p>
+            ) : null}
+          </div>
+
+
+
+            <button className='text-green-500' style={{width: '70px' }}
+              onClick={() => {
+                console.log("Sign in attempted")
+              }}
+            >Sign in</button>
+            <button className='text-yellow-500'  style={{width: '70px' }}
+              onClick={() => {
+                console.log("Sign up attempted")
+              }}
+            ><p>Sign up</p></button>
+            {/* <button className='text-red-500'  style={{width: '70px' }}
+              onClick={() => {
+                console.log("Red attempted")
+              }}
+            ><p>Red</p></button> */}
         </div>
     )
 }
