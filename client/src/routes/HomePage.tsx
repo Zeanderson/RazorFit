393ac9daa436a8eec34c65eb94bd45ed0cd96ae2
Zeanderson <zackanderson14@gmail.com>
2024-04-12 23:35:08 -0500

    Fixed Tailwind

--- client/src/routes/HomePage.tsx
+++ client/src/routes/HomePage.tsx
@@ -1,29 +1,27 @@
-import { useState } from 'react';
-function Home() {
-    const [count, setCount] = useState(0)
+
+
+function Header() {
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div className="flex flex-row rounded bg-darkRed gap-2 py-2 pl-5 pr-5 items-center relative max-h-0.5">
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+function Body() {
+    return (
+        <div>
+            {'Body'}
+        </div>
+    )
+}
+
+function Home() {
+    return (
+        <div className="flex flex-row gap-2">
+            <Header />
+            <Body />
+        </div>
     )
 }
 export default Home;
