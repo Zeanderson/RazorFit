70d7a693d9a2c8b3523e0405b7379de45b4a0a10
Zeanderson <zackanderson14@gmail.com>
2024-04-12 22:49:50 -0500

    Tailwind setup

--- client/src/routes/SignIn.tsx
+++ client/src/routes/SignIn.tsx
@@ -1,7 +1,9 @@
 function SignIn() {
     return (
-        <div>
-            {"Sign In"}
+        <div className="flex flex-col gap-2">
+            <button className='text-green-500'><p>Green</p></button>
+            <button className='text-yellow-500'><p>Yellow</p></button>
+            <button className='text-red-500'><p>Red</p></button>
         </div>
     )
 }
